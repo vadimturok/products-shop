34268f40eed3fb50b3ade092bb55958e502ed64f
turok <turokvadim2510@gmail.com>
2022-05-08 00:45:58 +0300

    .

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -38,7 +38,6 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, '../build'),
       filename: 'bundle.js',
-      publicPath: '/',
    },
    plugins: [
       new HtmlWebpackPlugin({
